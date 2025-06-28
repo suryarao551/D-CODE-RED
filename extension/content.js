daa130bdc0d8fa06256758a1d5f777aedd4b3467
KALA SURYA NARAYANA RAO <suryarao551@gmail.com>
2025-06-28 16:26:35 +0530

    Remove venv from repo and ignore it

--- extension/content.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("mouseup", function () {
-    const selectedText = window.getSelection().toString().trim();
-    if (selectedText.length > 0) {
-      chrome.runtime.sendMessage({ action: "text_selected", text: selectedText });
-    }
-  });
-  
